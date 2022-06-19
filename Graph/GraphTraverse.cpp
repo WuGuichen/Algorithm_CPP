19da9fa7a25d66c3977d0c4325fca619f5c58936
WuGuichen <577024128@qq.com>
2022-06-19 22:23:54 +0800

    拓扑排序

--- Graph/GraphTraverse.cpp
+++ Graph/GraphTraverse.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-void bfs(Node* node);
-void dfs(Node* node);
+void bfs(Node *node);
+void dfs(Node *node);
+list<Node *> sortedTopology(Graph graph);
 
-void bfs(Node* node){
-    if(node == nullptr) return;
-    queue<Node*> q;
-    unordered_set<Node*> set;  // 去重
+void bfs(Node *node) {
+    if (node == nullptr) return;
+    queue<Node *> q;
+    unordered_set<Node *> set; // 去重
     q.emplace(node);
     set.emplace(node);
-    while(!q.empty()){
-        Node* cur = q.front();
+    while (!q.empty()) {
+        Node *cur = q.front();
         q.pop();
-        printf("%d,",cur->value);
-        for(Node* next : cur->nexts){
-            if(set.find(next) == set.end()){
+        printf("%d,", cur->value);
+        for (Node *next : cur->nexts) {
+            if (set.find(next) == set.end()) {
                 set.emplace(next);
                 q.emplace(next);
             }
@@ -25,18 +26,18 @@ void bfs(Node* node){
     printf("\n");
 }
 
-void dfs(Node* node){
-    if(node == nullptr) return;
-    stack<Node*> stk;
-    unordered_set<Node*> set;
+void dfs(Node *node) {
+    if (node == nullptr) return;
+    stack<Node *> stk;
+    unordered_set<Node *> set;
     stk.emplace(node);
     set.emplace(node);
     printf("%d,", node->value);
-    while(!stk.empty()){
-        Node* cur = stk.top();
+    while (!stk.empty()) {
+        Node *cur = stk.top();
         stk.pop();
-        for(Node* next : cur->nexts){
-            if(set.find(next) == set.end()){
+        for (Node *next : cur->nexts) {
+            if (set.find(next) == set.end()) {
                 stk.emplace(cur);
                 stk.emplace(next);
                 set.emplace(next);
@@ -48,18 +49,42 @@ void dfs(Node* node){
     printf("\n");
 }
 
-int main(){
+list<Node *> sortedTopology(Graph graph) {
+    unordered_map<Node *, int> inMap;
+    queue<Node *> zeroInQueue;
+    for (auto value : graph.nodes) {
+        Node *node = value.second;
+        inMap.emplace(node, node->in);
+        if (node->in == 0) zeroInQueue.emplace(node);
+    }
+    list<Node *> res;
+    while (!zeroInQueue.empty()) {
+        Node *cur = zeroInQueue.front();
+        zeroInQueue.pop();
+        res.emplace_back(cur);
+        for (Node *next : cur->nexts) {
+            inMap[next]--; // 入度减一
+            if (inMap.at(next) == 0)
+                zeroInQueue.emplace(next);
+        }
+    }
+    return res;
+}
+
+int main() {
     vector<vector<int>> data(0, vector<int>(3));
     data = {{3, 7, 1},
             {3, 1, 2},
             {3, 6, 2},
             {6, 9, 1},
             {1, 9, 2},
-            {9, 7, 1},
-            {7, 3, 3}};
+            {9, 7, 1}};
     Graph graph(data);
-    Node* n = graph.getNode(3);
+    Node *n = graph.getNode(3);
     bfs(n);
     dfs(n);
+    list<Node *> topo = sortedTopology(graph);
+    cout << topo.size() << endl;
+    for (auto i : topo) printf("%d,", i->value);
     return 0;
 }
