692b9f26167447b61e32de5e5f4b80a8688df588
WuGuichen <577024128@qq.com>
2022-06-26 12:15:22 +0800

    Dijkstra算法

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -2,37 +2,38 @@
 
 using namespace std;
 
-unordered_map<Node*, int> dijkstra1(Node* head);
-Node* getMinDisAndUnselectedNode(unordered_map<Node*,int>,unordered_set<Node*> touchedNodes);
+unordered_map<Node *, int> dijkstra1(Node *head);
+Node *getMinDisAndUnselectedNode(unordered_map<Node *, int>, unordered_set<Node *> touchedNodes);
 
-unordered_map<Node*, int> dijkstra1(Node* head){
-    unordered_map<Node*, int> disMap;
+unordered_map<Node *, int> dijkstra1(Node *head) {
+    unordered_map<Node *, int> disMap;
     disMap.emplace(head, 0);
-    unordered_set<Node*> selectedNodes;  // 已经求过距离的Node
-    Node* minNode = getMinDisAndUnselectedNode(disMap, selectedNodes);
-    while(minNode != nullptr){   // 当无结点可选了跳出
+    unordered_set<Node *> selectedNodes; // 已经求过距离的Node
+    Node *minNode = getMinDisAndUnselectedNode(disMap, selectedNodes);
+    while (minNode != nullptr) { // 当无结点可选了跳出
         int dis = disMap.at(minNode);
-        for(Edge* edge : minNode->edges){
-            Node* toNode = edge->to;
-            if(disMap.find(toNode) == disMap.end()){
+        for (Edge *edge : minNode->edges) {
+            Node *toNode = edge->to;
+            if (disMap.find(toNode) == disMap.end()) {
                 disMap.emplace(toNode, dis + edge->weight);
+            } else {   // else减少新添加的节点重复比较最小值
+                int minDis = min(disMap.at(toNode), dis + edge->weight); // 原点距离加权重
+                disMap[edge->to] = minDis;
             }
-            int minDis = min(disMap.at(toNode), dis+edge->weight);  // 原点距离加权重
-            disMap[edge->to] = minDis;
         }
-        selectedNodes.emplace(minNode);  // 标记以求过该Node
+        selectedNodes.emplace(minNode); // 标记以求过该Node
         minNode = getMinDisAndUnselectedNode(disMap, selectedNodes);
     }
     return disMap;
 }
 
-Node* getMinDisAndUnselectedNode(unordered_map<Node*,int> disMap,unordered_set<Node*> touchedNodes){
-    Node* minNode = nullptr;
+Node *getMinDisAndUnselectedNode(unordered_map<Node *, int> disMap, unordered_set<Node *> touchedNodes) {
+    Node *minNode = nullptr;
     int minDis = INT_MAX;
-    for(auto m : disMap){
-        Node* node = m.first;
+    for (auto m : disMap) {
+        Node *node = m.first;
         int dis = m.second;
-        if(touchedNodes.find(node) == touchedNodes.end() && dis < minDis){
+        if (touchedNodes.find(node) == touchedNodes.end() && dis < minDis) {
             minNode = node;
             minDis = dis;
         }
@@ -40,12 +41,12 @@ Node* getMinDisAndUnselectedNode(unordered_map<Node*,int> disMap,unordered_set<N
     return minNode;
 }
 
-int main(){
+int main() {
     vector<vector<int>> data;
-    data = {{1, 3, 4},{3, 1, 4}, {2, 3, 8},{3, 2, 8}, {3, 4, 1},{4, 3, 1}, {1, 2, 3},{2,1,3}, {1, 4, 2},{4,1,2}};
+    data = {{1, 3, 4}, {3, 1, 4}, {2, 3, 8}, {3, 2, 8}, {3, 4, 1}, {4, 3, 1}, {1, 2, 3}, {2, 1, 3}, {1, 4, 2}, {4, 1, 2}};
     Graph graph(data);
-    unordered_map<Node*, int> disMap = dijkstra1(graph.getNode(3));
-    for(auto i : disMap){
+    unordered_map<Node *, int> disMap = dijkstra1(graph.getNode(1));
+    for (auto i : disMap) {
         printf("Node: %d, Distance: %d", i.first->value, i.second);
     }
     return 0;
